652f28f0a191e0c167cb44b1e1cbd90de1f050cc
saviorlana <savio.rvlana@gmail.com>
2024-02-06 14:44:57 -0300

    Criando a classe Posicao

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace xadrez_console
+﻿using System.Xml;
+using xadrez_console.Tabuleiro;
+
+namespace xadrez_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao p = new Posicao(3,4);
+            Console.WriteLine(p);
         }
     }
 }
